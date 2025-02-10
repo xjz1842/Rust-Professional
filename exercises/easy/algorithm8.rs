7aea5a7102bcc3f3cd003b1a6c19ebe1ed79ed55
Your Name <your.email@example.com>
2025-02-10 21:38:12 +0800

    algorithm8

--- exercises/easy/algorithm8.rs
+++ exercises/easy/algorithm8.rs
@@ -1,6 +1,6 @@
 /*
 	queue
-	This question requires you to use queues to implement the functionality of the stac
+	This question requires you to use queues to implement the functionality of the stack
 */
 
 
@@ -54,28 +54,43 @@ impl<T> Default for Queue<T> {
 
 pub struct myStack<T>
 {
-	//TODO
+	size:usize,
 	q1:Queue<T>,
 	q2:Queue<T>
 }
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
+			size:0,
 			q1:Queue::<T>::new(),
 			q2:Queue::<T>::new()
         }
     }
     pub fn push(&mut self, elem: T) {
-        //TODO
+        if self.size == 0 {
+            self.q1.enqueue(elem);
+            self.size += 1;
+        } else {
+             self.q2.enqueue(elem);
+             while let Ok(head) = self.q1.dequeue() {
+                   self.q2.enqueue(head);
+             }
+            // q2 -> q1
+            while let Ok(head) = self.q2.dequeue() {
+                self.q1.enqueue(head);
+            }
+            self.size += 1;
+        }
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        if self.is_empty() {
+           return  Err("Stack is empty");
+        }
+        self.size -= 1;
+		self.q1.dequeue()
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+        self.size == 0
     }
 }
 
